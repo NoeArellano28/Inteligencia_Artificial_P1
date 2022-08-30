2a6f3e4b00bfe7f06b0a464a2c8d227f674e2882
NoeArellano28 <111544515+NoeArellano28@users.noreply.github.com>
2022-08-30 13:52:49 -0500

    Practica5

---
+++ String4.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env phyton
+# _*_ coding: cp1252 _*_
+# _*_ coding: 850 _*_       #libreria para uso de acentos
+# _*_ coding: utf-8 _*_
+
+print("\tLenuajes de programación: \n-Phyton.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.\n-C++.\n-C#.") #Imprimir varias lineas en cmd con una sola en codigo
