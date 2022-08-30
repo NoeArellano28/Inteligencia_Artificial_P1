b073b6b429cc50bb9037a5f1ae08bff910c599be
NoeArellano28 <111544515+NoeArellano28@users.noreply.github.com>
2022-08-30 13:47:46 -0500

    Practica2

---
+++ Strings.py
@@ -0,0 +1,4 @@
+Variable = '"print()" se utiliza para imprimir valores en la consola'          # Escribo un string con comillas dobles dentro usando comillas simples
+Variable2 = "Para imprimir 'comillas simples' utilizo comillas dobles a los extremos" # Escribo un string con comillas simles dentro usando comillas dobles
+print(Variable) #imprimo la primer variable
+print(Variable2) #imprimo la segunda variable
