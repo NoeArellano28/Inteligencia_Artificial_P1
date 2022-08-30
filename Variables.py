95783d00c2b2d90c90a30f674bf7ae4595814f0d
NoeArellano28 <111544515+NoeArellano28@users.noreply.github.com>
2022-08-30 13:47:15 -0500

    Practica 1

---
+++ Variables.py
@@ -0,0 +1,6 @@
+mensaje = "Mi primer suma en phyton: "      # Creo mi variable de tipo string
+numero1 = 10                                # Creo mi primer variable numérica a sumar
+numero2 = 20                                # Creo mi segunda variable numérica a sumar
+suma = numero1 + numero2                    # realizo la suma de mis dos variables anteriores
+print (mensaje)                             # Imprimo mi primer variable (la tipo String)
+print (suma)                                # Imprimo el resulltado de la suma 
