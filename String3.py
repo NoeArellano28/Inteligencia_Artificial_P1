5b08bea9f6997759c0801e78c80edb1a9e2b1f4f
NoeArellano28 <111544515+NoeArellano28@users.noreply.github.com>
2022-08-30 13:50:35 -0500

    Practica4

---
+++ String3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env phyton
+# _*_ coding: cp1252 _*_
+# _*_ coding: 850 _*_       #libreria para uso de acentos
+# _*_ coding: utf-8 _*_
+
+titulo = "jesús noé arellano carrillo"      #uso de .title() para poner en mayúsculas la primera letra
+print(titulo.title())
+
+minusculas = "ORIGINALMENTE EN MAYUSCULAS"  #uso de .lower() para poner todo en minusculas
+print (minusculas.lower())
+
+minusculas = "originalmente en minusculas"  #uso de .upper() para poner todo en mayusculas
+print (minusculas.upper())
