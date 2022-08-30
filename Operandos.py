a64fbcdb8d4f6dd501dda1868b50a1a73ce0aa52
NoeArellano28 <111544515+NoeArellano28@users.noreply.github.com>
2022-08-30 13:53:28 -0500

    Practica6

---
+++ Operandos.py
@@ -0,0 +1,7 @@
+suma = 10 + 10      #operador suma de dos numeros
+resta = 10-10       #operador resta de dos numeros
+mult = 10*10        #operador multiplicacion de dos numeros
+div= 10/10          #operador division de dos numeros
+exp = 10**10        #operador exponencial de dos numeros
+
+print(" Suma:", suma,"\n","Resta:", resta, "\n" ,"Multipicacion:", mult,"\n","Division:", div,"\n", "Exponente:", exp ) #imprimimos los resultados 
