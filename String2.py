aa69a32c51c4bc96b78b814498d72b8644fa08d8
NoeArellano28 <111544515+NoeArellano28@users.noreply.github.com>
2022-08-30 13:50:02 -0500

    Practica3

---
+++ String2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env phyton
+# _*_ coding: cp1252 _*_
+# _*_ coding: 850 _*_       #libreria para uso de acentos
+# _*_ coding: utf-8 _*_
+
+con = "Hola " + "Mundo"     #Concatenando dos strings con el signo + en una sola variable
+print (con)
+
+x = "Esta es mi primera "   #Concatenando dos strings, una en cada variable 
+y = "concatenación"
+con2 = x + y
+print (con2)
+
+nombre = "Jesús Noé "       # Concatenando mi nombre completo 
+apellido = "Arellano Carrillo"
+completo = nombre + apellido
+print(completo)
+
+print ("2"+"8")             #Concatenando dos numeros sin sumarlos
